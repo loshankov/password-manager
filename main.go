e02ccb47742c1f25536b05cd819040bb7b046de2
Nikita Loshankov <loshankov.n@gmail.com>
2026-07-08 19:22:51 +0300

    add NewPassword func

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "time"
+
+type Password struct {
+	Name         string    `json:"name"`
+	Value        string    `json:"value"`
+	Category     string    `json:"category"`
+	CreatedAt    time.Time `json:"createdAt"`
+	LastModified time.Time `json:"lastModified"`
+}
+
+func NewPassword(name, value, category string) Password {
+	return Password{
+		Name:         name,
+		Value:        value,
+		Category:     category,
+		CreatedAt:    time.Now(),
+		LastModified: time.Now(),
+	}
+}
 func main() {
 
 }
